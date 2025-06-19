5750a4c72dc6daecc12934e088b4f8f9ed068106
Michelle <jiahu0724417@gmail.com>
2025-06-19 22:36:18 +0800

    修改圖片路徑vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => ({
   base: mode === 'production' ? '/ootd-factory/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // ← 這行要有！
+    },
+  },
 }))
