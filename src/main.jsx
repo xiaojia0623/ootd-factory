dfa220b06713374034e24966610d09b67f16c450
Michelle <jiahu0724417@gmail.com>
2025-06-19 21:44:09 +0800

    調整插入元件AppWithProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/all.scss'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'swiper/css';
-import App from './App.jsx'
 import axios from 'axios'
+import 'swiper/css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './styles/all.scss'
+import AppWithProvider from './pages/AppWithProvider';
+
+
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <AppWithProvider />
   </StrictMode>,
 )
+
+
+
