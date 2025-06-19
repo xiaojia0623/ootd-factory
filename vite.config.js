7fa35fa00f2f3c0597d04ead76699c45ce8cd285
Michelle <jiahu0724417@gmail.com>
2025-06-19 21:55:08 +0800

    修改process

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ootd-factory/' : '/',
+export default defineConfig(({mode}) => ({
+  base: mode === 'production' ? '/ootd-factory/' : '/',
   plugins: [react()],
-})
+}))
